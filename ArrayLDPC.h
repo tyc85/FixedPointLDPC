36f829dbba587ed353070f5acc6be822118b50d4
tyc85 <steven.tychen@gmail.com>
2014-01-31 13:19:20 -0600

    add time trials version with windows time measurement

--- ArrayLDPC.h
+++ ArrayLDPC.h
@@ -1,5 +1,5 @@
 #ifndef PROTO_LDPC_H
 #define PROTO_LDPC_H
-int ArrayLDPC(double db_start, double db_end, double db_step, char* Filename);
-
+int ArrayLDPC_PerfTest(double db_start, double db_end, double db_step, char* Filename);
+int ArrayLDPC_TimeTrial(double db, int MaxPckNum, char* Filename);
 #endif
